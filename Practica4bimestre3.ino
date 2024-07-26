f40527650dde83b657c1ff7ce3dfe960bed35c04
MidsEureka <167281788+MidsEureka@users.noreply.github.com>
2024-07-26 13:53:57 -0600

    Practica4bimestre3.ino

--- Practica4bimestre3.ino
+++ Practica4bimestre3.ino
@@ -15,7 +15,7 @@
 
 #define RST_PIN 9
 #define SS_PIN 10
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 #define LED_VERDE 2
 #define LED_ROJO 3
@@ -36,17 +36,14 @@ void setup() {
 }
 
 void loop() {
-  // Look for new cards
   if (!mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
 
-  // Select one of the cards
   if (!mfrc522.PICC_ReadCardSerial()) {
     return;
   }
 
-  // Read the UID of the card
   String tagUID = "";
   for (byte i = 0; i < mfrc522.uid.size; i++) {
     tagUID += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
@@ -56,21 +53,19 @@ void loop() {
 
   Serial.println("UID tag : " + tagUID);
 
-  // Example UID for registered tag
   String registeredUID = "4312D41C";
 
   if (tagUID == registeredUID) {
-    // Correct tag
+     
     digitalWrite(LED_VERDE, HIGH);
     tone(BUZZER, 200);
     delay(4000); // Keep LED and buzzer active for 4 seconds
     digitalWrite(LED_VERDE, LOW);
     noTone(BUZZER);
   } else {
-    // Incorrect tag
     digitalWrite(LED_ROJO, HIGH);
     tone(BUZZER, 400);
-    delay(2000); // Keep LED and buzzer active for 2 seconds
+    delay(2000);
     digitalWrite(LED_ROJO, LOW);
     noTone(BUZZER);
   }
